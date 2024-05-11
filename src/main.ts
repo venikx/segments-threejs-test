c3a8fa77ac63c1ba9e7cc749982245e37a0334fa
Kevin Rangel <code@venikx.com>
2024-05-11 13:06:07 +0200

    refactor: move creation into functions

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,8 @@ import "./style.css"
 import * as THREE from "three"
 
 function main() {
-  const canvas = document.querySelector("#c")
-  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
-  renderer.setSize(window.innerWidth, window.innerHeight)
-
-  const camera = new THREE.PerspectiveCamera(
-    45, // field of view
-    window.innerWidth / window.innerHeight, // aspect ratio
-    1, // near the plane
-    1000 // far from the plane => so is this maximum drawing distance?
-  )
-  camera.position.z = 2
-
-  const scene = new THREE.Scene()
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 })
-  const cube = new THREE.Mesh(geometry, material)
-  scene.add(cube)
+  const { canvas, renderer, camera, scene } = createWorld()
+  const cube = createCube(scene)
 
   function render(time) {
     time *= 0.001 // convert time to seconds
@@ -31,6 +16,40 @@ function main() {
   }
 
   requestAnimationFrame(render)
+}
+
+function createWorld() {
+  const canvas = document.querySelector("#c")
+  if (!canvas) throw new Error("No canvas element to hook into.")
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  const camera = new THREE.PerspectiveCamera(
+    45, // field of view
+    window.innerWidth / window.innerHeight, // aspect ratio
+    1, // near the plane
+    1000 // far from the plane => so is this maximum drawing distance?
+  )
+  camera.position.z = 2
+
+  const scene = new THREE.Scene()
+
+  return {
+    canvas,
+    renderer,
+    camera,
+    scene,
+  }
+}
+
+function createCube(scene: THREE.Scene) {
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
+  return cube
 }
 
 main()
