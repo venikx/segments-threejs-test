d22f17494e53016072e356b146e1620158bce91f
Kevin Rangel <code@venikx.com>
2024-05-11 12:49:27 +0200

    feat: hello world threejs

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css"
+import * as THREE from "three"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function main() {
+  const canvas = document.querySelector("#c")
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
+  renderer.setSize(window.innerWidth, window.innerHeight)
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  const camera = new THREE.PerspectiveCamera(
+    45, // field of view
+    window.innerWidth / window.innerHeight, // aspect ratio
+    1, // near the plane
+    1000 // far from the plane => so is this maximum drawing distance?
+  )
+  camera.position.z = 2
+
+  const scene = new THREE.Scene()
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
+  function render(time) {
+    time *= 0.001 // convert time to seconds
+
+    cube.rotation.x = time
+    cube.rotation.y = time
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(render)
+  }
+
+  requestAnimationFrame(render)
+}
+
+main()
